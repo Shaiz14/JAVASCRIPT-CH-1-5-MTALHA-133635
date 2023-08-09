9835c8438c242a7f127134735ead13eaf483053e
Shaiz14 <talhafreelance14@gmail.com>
2023-08-09 21:08:53 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,136 @@
+                                            // Assignment Chapter 01
+// // Q No.1
+// alert("welcome")
+
+// // Q Node.2
+// alert("Error! Please enter a valid password")
+
+// // Q Node.3
+// alert("Welome to JS land...\n Happy Coding")
+
+// // Q Node.4
+// alert("Welome to JS land")
+// alert("Happy Coding!")
+
+// // Q Node.5
+// console.log("Hello! I can run JS through my browser")
+
+//                                             // Assignment Chapter 02
+//                                             // Q No.1
+// var username;
+//                                             // Q No.2
+// var myname = "Muhammad Talha";
+//                                             // Q No.3
+// var message;
+// message = "Hello World"
+// alert(message)
+                                            // Q No.4
+// var studentBioData;
+// studentBioData = "john doe"
+// alert(studentBioData)
+
+// var Age;
+// Age = "15 year old"
+// alert(Age)
+
+// var certified;
+// certified ="certififed mobile application developer"
+// alert(certified)
+
+                                            // Q No.5
+
+// var JS_variable;
+// JS_variable = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(JS_variable);
+                                            // Q No.6
+
+// var email;
+// email = "example@example,.com";
+// alert("My email address is" + email);
+                                            // Q No.7
+// var book;
+// book = "A smarter way to learn JAVA Script";
+// alert("I am trying to learn from a book." + book);
+
+                                            // Q No.8
+// document.write("Yah! i can write HTML content through JAVA Script")
+
+                                            // Q No.9
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+                                            
+                                            // CHAPTER Node.03
+                            
+                                            // Q Node.01
+// var age;
+// age = " I am 15 year old"
+// alert(age)
+                                            // Q Node.02
+// var N;
+// N = 14;
+// alert("You have visited this site "+ N + " times")
+
+//                                             // Q Node.03
+// var birth_year;
+// birth_year = 1990;
+// alert("My birth year is " + birth_year)
+//                                             // Q Node.04
+// var visitor_Name;
+// Visitor_Name = "John doe";
+// var Product_title;
+// Product_title = "5 T-Shirt ";
+// var order;
+// order = "XYZ Clothing Store";
+// alert(Visitor_Name + " ordered " + Product_title +" on " + order)
+
+//                                             // CHAPTER 04
+
+                                                // Q Node.01
+// var var1, var2, var3;
+// var1 = "Talha";
+// Var2 = 123
+// var3 = "Talha14"
+
+                                                // Q Node.02
+
+//                                                 // 05 x legal variable
+// var variable_legal;
+// variable_legal = "talha, 15 year, male, muhammadTalha, muhammad_talha";
+// alert(variable_legal);
+//                                                 // 05 x illegal variable
+// var variable_Illegal;
+// variable_Illegal = "3xtalha, 15year, 2xmale, MuhammadTalha, Muhammadtalha";
+// alert(variable_Illegal);
+                                                // Q No.03
+//   var dash1 = "Talha";
+//   var dash2 ="133635";
+//   var dash3 = "$";
+//   var dash4= "SMIT";
+//   document.write (" Variable names can only contain " + dash1 + " " + dash2 + " " + dash3 + " and " + dash4 )
+  
+                                                // Q No.04
+// var dash5 = " Sensitive";
+// document.write ( " </br> Variable names are case " +dash5 )
+
+                                                // Q No.05
+// var dash5 = " Keywords";
+    // document.write ( " </br> Variable names should not be JS " +dash5 )
+
+                                                // ASSIGNMENT 05
+            
+                                                    // Q NO.01
+    //    var num= 3;
+    //         var num1= 5;
+    //         var num3= num+num1;
+    //         document.write (" sum of " + num +" "+ "and " + num1 +  " is" + " "  + num3)
+    //         var num= 3;
+    //         var num1= 5;
+    //         var num3= num*num1;
+    //         document.write (" </br>  sum of " + num +" "+ "Multiply " + num1 +  " is" + " "  + num3)
+    //         var num= 10;
+    //         var num1= 2;
+    //         var num3= num/num1;
+    //         document.write (" </br>  sum of " + num +" "+ "Divied " + num1 +  " is" + " "  + num3)
+
+                                                    
+
+
